a4d732dbf55e4d7f77113ae124b3bdd765fc2be6
Shamim <imsiddiquee@gmail.com>
2019-04-09 15:15:43 +0600

    Pessimistic UI illusion for better UI experience

--- src/App.js
+++ src/App.js
@@ -34,10 +34,18 @@ class App extends Component {
   };
 
   handleDelete = async post => {
-    await axios.delete(apiEndpoint + "/" + post.id);
+    const originalPost = this.state.posts;
 
     const posts = this.state.posts.filter(p => p.id !== post.id);
     this.setState({ posts });
+
+    try {
+      await axios.delete(apiEndpoint + "/" + post.id);
+      //throw new Error("");
+    } catch (ex) {
+      alert("Something failed while deleting a post!");
+      this.setState({ posts: originalPost });
+    }
 
     console.log("Deleted");
   };
